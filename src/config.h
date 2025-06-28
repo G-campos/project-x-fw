c8f09069bf99d649fab4008a9c789366521a599b
gabriel_gonsalves <gabriel.gonsalves@inova.ind.br>
2025-06-27 23:30:19 -0300

    refactor: atribuindo somente dados de contexto global

--- src/config.h
+++ src/config.h
@@ -5,14 +5,10 @@
 #include <cstddef>
 
 #include "utils/logger.h"
+#include "storage/storage.h"
 
 extern Logger logger;
-
-// WIFI config
-
-#define AP_SSID "ESP-CONFIG"
-#define AP_PASS ""
-#define WIFI_STORAGE_NAMESPACE "wifi_config"
+extern Config config;
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 99
@@ -26,40 +22,5 @@ extern Logger logger;
 
 #define ON         0x07E0
 #define OFF      0xF800
-
-// THINGSBOARD CONFIG
-
-#if defined(ESP8266)
-#define THINGSBOARD_ENABLE_PROGMEM 0
-#endif
-
-// See https://thingsboard.io/docs/getting-started-guides/helloworld/
-// to understand how to obtain an access token
-constexpr char TOKEN[] = "YOUR_ACCESS_TOKEN";
-
-// Thingsboard we want to establish a connection too
-constexpr char THINGSBOARD_SERVER[] = "demo.thingsboard.io";
-// MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port.
-constexpr uint16_t THINGSBOARD_PORT = 1883U;
-
-// Maximum size packets will ever be sent or received by the underlying MQTT client,
-// if the size is to small messages might not be sent or received messages will be discarded
-constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
-
-// Baud rate for the debugging serial connection.
-// If the Serial output is mangled, ensure to change the monitor speed accordingly to this variable
-constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
-
-// Maximum amount of attributs we can request or subscribe, has to be set both in the ThingsBoard template list and Attribute_Request_Callback template list
-// and should be the same as the amount of variables in the passed array. If it is less not all variables will be requested or subscribed
-constexpr size_t MAX_ATTRIBUTES = 3U;
-
-constexpr uint64_t REQUEST_TIMEOUT_MICROSECONDS = 5000U * 1000U;
-
-// Attribute names for attribute request and attribute updates functionality
-
-constexpr const char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
-constexpr const char LED_MODE_ATTR[] = "ledMode";
-constexpr const char LED_STATE_ATTR[] = "ledState";
 
 #endif // CONFIG_H
